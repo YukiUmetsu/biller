df025e6679c46ccb4effa95d0e7d4849df6b58a3
YukiUmetsu <yuuki.umetsu@gmail.com>
2016-05-02 00:03:34 +0900

    added accounts controller, index, show

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < Account
   has_many :time_entries
+  has_many :projects
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501125725) do
+ActiveRecord::Schema.define(version: 20160501145652) do
 
   create_table "account_entries", force: :cascade do |t|
-    t.float    "time",        limit: 24, null: false
+    t.float    "time",        limit: 24,  null: false
     t.integer  "customer_id", limit: 4
     t.integer  "employee_id", limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "account_id",  limit: 4
+    t.string   "type",        limit: 255
   end
 
   create_table "accounts", force: :cascade do |t|
@@ -31,6 +32,20 @@ ActiveRecord::Schema.define(version: 20160501125725) do
     t.string   "address",    limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "employees_projects", force: :cascade do |t|
+    t.integer  "employee_id", limit: 4
+    t.integer  "project_id",  limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.integer  "customer_id", limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
 end
